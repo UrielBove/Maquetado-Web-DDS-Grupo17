aad88cdb06ba6459b01090913091defd1f80860b
FrancoPadovani <fpadovani@frba.utn.edu.ar>
2025-09-08 17:46:41 -0300

    Falataba actualizar algo

--- contribuyente_vista/visualizar_hecho_script.js
+++ contribuyente_vista/visualizar_hecho_script.js
@@ -84,3 +84,17 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+
+const profileBtn = document.getElementById("profileBtn");
+const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
+
+profileBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+  overlay.classList.toggle("active");
+});
+overlay.addEventListener("click", () => {
+  sidebar.classList.remove("active");
+  overlay.classList.remove("active");
+});
