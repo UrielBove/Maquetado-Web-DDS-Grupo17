edb29f747fd9907a6eea3733a87172fa3e21ab83
FrancoPadovani <fpadovani@frba.utn.edu.ar>
2025-09-08 01:44:40 -0300

    Formatee y agregue colores para call to action

--- administrador_vista/script.js
+++ administrador_vista/script.js
@@ -1,12 +1,12 @@
-    const profileBtn = document.getElementById("profileBtn");
-    const sidebar = document.getElementById("sidebar");
-    const overlay = document.getElementById("overlay");
+const profileBtn = document.getElementById("profileBtn");
+const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
 
-    profileBtn.addEventListener("click", () => {
-      sidebar.classList.toggle("active");
-      overlay.classList.toggle("active");
-    });
-    overlay.addEventListener("click", () => {
-      sidebar.classList.remove("active");
-      overlay.classList.remove("active");
-    });
+profileBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+  overlay.classList.toggle("active");
+});
+overlay.addEventListener("click", () => {
+  sidebar.classList.remove("active");
+  overlay.classList.remove("active");
+});
